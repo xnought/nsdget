e27814176ca4a0b250dc5d0f14592d14934f9514
xnought <drbertucci@hotmail.com>
2025-03-09 21:41:19 -0400

    open up stimuli as imge

--- nsdget/__init__.py
+++ nsdget/__init__.py
@@ -1,6 +1,7 @@
 import wget
 import pandas as pd
 import os
+from PIL import Image
 
 BASE_URL = "https://natural-scenes-dataset.s3.amazonaws.com"
 COCO_BASE_URL = "http://images.cocodataset.org"
@@ -40,10 +41,14 @@ def left_pad_zeros(number: int, pad_to=12):
     return "0" * num_zeros + number_as_str
 
 
+def coco_filename(id: int):
+    return f"{left_pad_zeros(id, pad_to=12)}.jpg"
+
+
 def coco_image_links(coco_ids: list[int], splits: list[str]):
     assert len(coco_ids) == len(splits)
     for id, split in zip(coco_ids, splits):
-        filename = f"{left_pad_zeros(id, pad_to=12)}.jpg"
+        filename = coco_filename(id)
         yield f"{COCO_BASE_URL}/{split}/{filename}", filename
 
 
@@ -73,12 +78,38 @@ def download_coco_links(coco_ids: list[int], splits: list[str]):
 
     # download to directories
     links = []
-    save_paths = []
+    relative_paths = []
     for (link, filename), split in zip(coco_image_links(coco_ids, splits), splits):
         links.append(link)
-        save_paths.append(os.path.join(DATA_DIR, split, filename))
+        relative_paths.append(os.path.join(split, filename))
 
-    parallel_wget(links, save_paths)
+    parallel_wget(links, [os.path.join(DATA_DIR, r) for r in relative_paths])
+
+    return relative_paths
+
+
+def percent_crop_image(im: Image, percent_crop: list[float]):
+    # percent crop is (top, bottom, left, right)
+    [percent_top, percent_bottom, percent_left, percent_right] = percent_crop
+
+    # but PIL.Image().crop takes in (left, top, right, bottom)
+    width, height = im.size
+    left = int(width * percent_left)
+    top = int(height * percent_top)
+    right = int(width * (1 - percent_right))
+    bottom = int(height * (1 - percent_bottom))
+    return im.crop([left, top, right, bottom])
+
+
+def load_image(coco_id: int, coco_split: str, crop: list[float] = None) -> Image:
+    filename = coco_filename(coco_id)
+    path = os.path.join(DATA_DIR, coco_split, filename)
+    assert os.path.exists(path)
+
+    im = Image.open(path).convert("RGB")
+    if crop is not None:
+        im = percent_crop_image(im, crop)
+    return im
 
 
 def main():
@@ -86,7 +117,9 @@ def main():
     # download_images()
     df = df_stimuli_info()
     sub = df[df["shared1000"] == True]
-    download_coco_links(sub["cocoId"], sub["cocoSplit"])
+    imgs = download_coco_links(sub["cocoId"], sub["cocoSplit"])
+    row = sub.iloc[0]
+    load_image(row["cocoId"], row["cocoSplit"], row["cropBox"]).show()
 
 
 if __name__ == "__main__":
